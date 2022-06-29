eb6f3df1d6e9828edbbb6aff976fc0eb04d681b2
Stephen Lynch <stephen.lynch17@icloud.com>
2022-06-29 09:42:33 +0000

    Linked emailjs to project

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+// Note the first parameter is our emailjs service ID, the second is the template name and the third is an object
+// that contains the template parameters.
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
